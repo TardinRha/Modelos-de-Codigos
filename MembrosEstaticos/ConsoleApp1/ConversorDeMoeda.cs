249c32fd79042bc787e1103c91879d1700fb3a70
Rhaissa Tardin <rha.tardin@gmail.com>
2023-03-02 16:05:48 -0300

    Exercicio com membro estático

---
+++ MembrosEstaticos/ConsoleApp1/ConversorDeMoeda.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class ConversorDeMoeda
+    {
+        public static double Conversor(double cotacaoDolar, double valorComprar)
+        {
+            return (valorComprar * cotacaoDolar) + ((valorComprar * cotacaoDolar) * 0.06);
+        }
+    }
+}
---
+++ MembrosEstaticos/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using ConsoleApp1;
+using System.Globalization;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Qual é a cotação do dólar? ");
+            double cotacaoDolar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantos dolares você vai comprar? ");
+            double valorCompra = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Valor a ser pago em reais = ");
+            Console.WriteLine(ConversorDeMoeda.Conversor(cotacaoDolar, valorCompra).ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
+ 
